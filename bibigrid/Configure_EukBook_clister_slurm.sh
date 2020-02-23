7cd786f8ea4f8001369b8a3d00c3cd243dc88690
dembra96 <dembra96@gmail.com>
2020-02-23 17:08:19 +0100

    Update MetaEuk in installation script 3bcb48a > 48a4b0f
    NOTE: Results will be saved to the same metaeuk_alpha folder.
    
    For the Snakemake pipeline Metaeuk version stays the same and
    results are not recalculated. Eli said the prediction algorithm did not
    change much and should not affect the results.
    
    Just to know: first five sets were calculated by the previous version.
    All next sets will be calculated using the new version.

--- bibigrid/Configure_EukBook_clister_slurm.sh
+++ bibigrid/Configure_EukBook_clister_slurm.sh
@@ -69,7 +69,7 @@ conda clean -a -y
 echo -e "\n\n[[ INSTALL METAEUK ]]\n"
 
 if [[ ! -f "successfully_installed_metaeuk" ]]; then
-	while true; do wget --timeout=10 -c https://mmseqs.com/archive/3bcb48a877c3074fb72f42227be361f0c7a29cb9/metaeuk-linux-avx2.tar.gz && break; done
+	while true; do wget --timeout=10 -c https://mmseqs.com/archive/48a4b0f58fd82bd1e690c96d18c31c8d63e141dd/metaeuk-linux-avx2.tar.gz && break; done
 	tar xzvf metaeuk-linux-avx2.tar.gz
 	PATH=$PATH:${HOME}/metaeuk/bin/
 	metaeuk
