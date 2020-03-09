78bff3cbb38a36910cbb76663899e065c8a64976
dembra96 <dembra96@gmail.com>
2020-03-09 14:26:52 +0000

    Implement Megahit_parallel_mode as config option
    Big samples keep failing the pipeline. I decided to run them with "fix"
    snakemake script that has maximum resources allowed to the megahit.
    Thus, it is with max memory and not parallel.
    
    Additionally, I wanted to keep the parallel option as default to all
    other sets. So, I decided to put megahit_parallel execution as an
    option. In my config now it is a default True. Fix script overwrites it
    to be False.

--- bibigrid/Fixing_fails_slurm.sh
+++ bibigrid/Fixing_fails_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --array=11,67
 #SBATCH --nodelist=bibigrid-worker1-23-nxscftyhrss47ez
 echo "SLURM_ARRAY_TASK_ID is:  ${SLURM_ARRAY_TASK_ID}"
-echo "This is a continuation after errors of 100 sets. Current set had an error."
+echo "This is a continuation after errors of 150 sets and wrong raw QUASTS. Current set had an error."
 source ~/.profile #to activate conda 
 
 workdir="/mnt/samples/simple"
@@ -14,6 +14,6 @@ mkdir -p $workdir
 sample_yaml_dir="/home/ubuntu/vol/spool/new_accession_yamls"
 
 cd ~/EukBook
-./Snakemake --configfile ${sample_yaml_dir}/set_${SLURM_ARRAY_TASK_ID}.yaml --config Quick_mode=False -k 
+./Snakemake --configfile ${sample_yaml_dir}/set_${SLURM_ARRAY_TASK_ID}.yaml --config Quick_mode=False Megahit_parallel_mode=False -k 
 
 # -k - keep going with independent jobs if a job fails. Useful for quick runs where quasts may fail.
